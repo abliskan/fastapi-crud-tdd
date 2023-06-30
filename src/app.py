35d1095c3e448cc2129f6bb135e790a10e04232f
RickyS <suhanryricky@yahoo.com>
2023-06-30 21:41:55 +0700

    Add: init file inside src and tests folder

--- src/app.py
+++ src/app.py
@@ -5,5 +5,6 @@ app = FastAPI()
 # run: pipenv run uvicorn src.main:app
 @app.get("/")
 def hello():
+    # app_message = os.environ.get("APP_MESSAGE", "Hello")
     return {"message": "Hello World!"}
 
