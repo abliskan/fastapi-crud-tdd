a4b937910363ab6b64b08f50a66b747bf05d7e59
RickyS <suhanryricky@yahoo.com>
2023-07-06 14:45:47 +0700

    Updated: models at src/models.py

--- src/app.py
+++ src/app.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
-# run: pipenv run uvicorn src.main:app | uvicorn src.app:app
+# run: pipenv run uvicorn src.app:app | uvicorn src.app:app
 @app.get("/")
 def hello():
     # app_message = os.environ.get("APP_MESSAGE", "Hello")
     return {"message": "Hello World!"}
+
 
