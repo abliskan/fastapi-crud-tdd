de9c205625b8d594fd7c8156e0f4328aedd2d506
RickyS <suhanryricky@yahoo.com>
2023-07-12 16:55:46 +0700

    Updated:model.py

--- src/models.py
+++ src/models.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel
 from typing import Optional
 from uuid import UUID, uuid4
+from datetime import date, datetime
 
 
 class Product(BaseModel):
     id: Optional[UUID] = uuid4
     name: str
     price: int
-    created_by: Optional[str]= None
-    dated_created: Optional[str] = None
+    created_by: Optional[str] = None
+    created_at: datetime = datetime.now()
